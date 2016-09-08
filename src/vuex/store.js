a93fc3e2a1e53a9313f8b2ad11b927c00dcb069f
easonxie <278255596@qq.com>
2016-09-08 09:48:52 +0800

    delete file

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -5,13 +5,11 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import $ from 'jquery'
 import ListConfig from './List/config'
-import DetailConfig from './Detail/config'
-import EditorBannerConfig from './EditorBanner/config'
 Vue.use(Vuex);
 
-const state = $.extend({}, ListConfig.state, DetailConfig.state, EditorBannerConfig.state);
+const state = $.extend({}, ListConfig.state);
 
-const mutations = $.extend({}, ListConfig.motations, DetailConfig.motations, EditorBannerConfig.motations);
+const mutations = $.extend({}, ListConfig.motations);
 
 export default new Vuex.Store({
   state,
